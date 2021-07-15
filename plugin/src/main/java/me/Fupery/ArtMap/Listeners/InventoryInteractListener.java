9eca56583a4674fe0621bb98e43de3a67728106f
James Vox <downfall_bassplayer@msn.com>
2021-07-15 04:40:03 +0000

    Update InventoryInteractListener.java fixed armor loss while at the easel

--- plugin/src/main/java/me/Fupery/ArtMap/Listeners/InventoryInteractListener.java
+++ plugin/src/main/java/me/Fupery/ArtMap/Listeners/InventoryInteractListener.java
@@ -38,6 +38,12 @@ class InventoryInteractListener implements RegisteredListener {
                 event.setCancelled(true);
             }
         }
+        // lock armor slots while at the easel
+        else if (ArtMap.instance().getArtistHandler().getCurrentSession((Player) event.getWhoClicked()) != null &&
+            event.getRawSlot() >= 5 && event.getRawSlot() <= 8){
+            event.setCancelled(true);
+        }
+
     }
 
     @EventHandler
